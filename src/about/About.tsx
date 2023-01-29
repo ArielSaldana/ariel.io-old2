6b045a86d7fb4597f0f8310dab1e20f237612494
Ariel Saldana <ariel@unreal.io>
2023-01-28 20:13:09 -0500

    comment out old about page

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,12 +1,12 @@
-import Navigation from "../components/navigation/navigation";
-import ReactMarkdown from 'react-markdown'
-
-export default function About() {
-    return (
-        <div className="ariel-about">
-            <Navigation></Navigation>
-            <h1>My About Page</h1>
-            <ReactMarkdown># Hello, *world*!</ReactMarkdown>
-        </div>
-    )
-}
+//import Navigation from "../components/navigation/navigation";
+//import ReactMarkdown from 'react-markdown'
+//
+//export default function About() {
+//    return (
+//        <div className="ariel-about">
+//            <Navigation></Navigation>
+//            <h1>My About Page</h1>
+//            <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+//        </div>
+//    )
+//}
--- src/about/_about.tsx
+++ src/about/_about.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import About from "./About";
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <About/>
-    </React.StrictMode>,
-)
+//import React from 'react'
+//import ReactDOM from 'react-dom/client'
+//
+//import About from "./About";
+//
+//ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//    <React.StrictMode>
+//        <About/>
+//    </React.StrictMode>,
+//)
